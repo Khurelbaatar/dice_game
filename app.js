a717f6a5a4537cc1026ef8ace655f8a45d98f73c
khurelbgaatar <xvrlee.lh@gmail.com>
2019-12-07 20:08:14 +0800

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -8,14 +8,17 @@ var scores = [0, 0];
 var roundScore = 0;
 // Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй. 1-6 гэсэн утгыг энэ хувьсагчид
 
-var dice = Math.floor(Math.random() * 6) + 1;
+document.getElementById("score-0").textContent = "0"; // DOM oos zovhon ID haidag uchraas iluu hurdan ajillan
+document.getElementById("score-1").textContent = "0";
 
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
-document.querySelector(".dice").style.display = "none";
-
-console.log("Шоо :" + dice);
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
